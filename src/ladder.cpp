26b8f7295e3f67cfd83c225819607a1d1bc2bae5
Ranoldt <ronaldntran12@gmail.com>
2025-03-14 18:42:29 +0000

    world ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -17,6 +17,33 @@ bool is_adjacent(const string& word1, const string& word2) {
     int diff = 0;
     for (int i = 0; i < len; ++i) {
         if (word1[i] != word2[i]) {++diff;}
+        if (diff > 1) {return false;}
     }
     return difference == 1;
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    queue<vector<string>> lq;
+    lq.push({begin_word});
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while (!lq.empty()) {
+        vector<string> ladder = lq.front();
+        lq.pop();
+        string last_word = ladder.back();
+        for (string word : word_list) {
+            if (is_adjacent(last_word, word)) {
+                if (!visited.contains(word)) {
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word)
+                        return new_ladder;
+                    lq.push(new_ladder); 
+                }
+            }
+        }
+    }
+    return vector<string>();
 }
